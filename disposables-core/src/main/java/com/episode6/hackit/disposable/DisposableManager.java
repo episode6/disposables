64987ce86621e435cb5b5c8d23ba428e1cdbc22c
Geoff Hackett <ghackett@gmail.com>
2017-06-08 09:39:14 -0400

    javadoc punctuation

--- disposables-core/src/main/java/com/episode6/hackit/disposable/DisposableManager.java
+++ disposables-core/src/main/java/com/episode6/hackit/disposable/DisposableManager.java
@@ -9,14 +9,14 @@ public interface DisposableManager extends HasDisposables {
 
   /**
    * Add a disposable to this manager, instructing the manager to pass down its calls to {@link #dispose()} to
-   * the new disposable
-   * @param disposable The disposable to add
+   * the new disposable.
+   * @param disposable The disposable to add.
    */
   void add(Disposable disposable);
 
   /**
    * Add a collection of disposables to this manager, instructing the manager to pass down its calls
-   * to {@link #dispose()} to all disposables in the provided collection
+   * to {@link #dispose()} to all disposables in the provided collection.
    * @param disposables a collection containing disposable to add.
    */
   void addAll(Collection<Disposable> disposables);
