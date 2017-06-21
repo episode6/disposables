61ea81d787dc6150944139cf5a454c5c1aad9b0a
Geoff Hackett <ghackett@gmail.com>
2017-06-21 09:37:18 -0400

    javadoc validation!

--- pausables-core/src/main/java/com/episode6/hackit/pausable/PausableDisposableManager.java
+++ pausables-core/src/main/java/com/episode6/hackit/pausable/PausableDisposableManager.java
@@ -3,7 +3,7 @@ package com.episode6.hackit.pausable;
 import com.episode6.hackit.disposable.DisposableManager;
 
 /**
- * Interface for a manager of a collection of pausables & disposables. Any pausables
+ * Interface for a manager of a collection of pausables and disposables. Any pausables
  * added to a PausableDisposableManager should also be disposed of / flushed if
  * they happen to implement {@link com.episode6.hackit.disposable.Disposable} or
  * {@link com.episode6.hackit.disposable.CheckedDisposable}. The inverse should
